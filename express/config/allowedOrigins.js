1809d330de01d76448672f9c26506f96fc0d9b51
Todd <thickam@pdx.edu>
2023-03-02 20:04:07 -0800

    added background image url to body

--- express/config/allowedOrigins.js
+++ express/config/allowedOrigins.js
@@ -2,7 +2,6 @@ const allowedOrigins = [
   "https://www.yoursiteDomain.com",
   "https://google.com",
   // the following are for dev and should be removed
-  "https://stackblitz.com/github/hickamt/HTTP_Only_App_Template/",
   "http://127.0.0.1:5500/",
   "http://localhost:5500/",
   "http://localhost:5173",
