37a626c4f01f1ad549ec8288cbde45d327a24d3c
Todd <thickam@pdx.edu>
2023-03-04 14:07:11 -0800

    updating corsOptions to allow StackBlitz deployment

--- express/config/corsOptions.js
+++ express/config/corsOptions.js
@@ -1,4 +1,5 @@
 const allowedOrigins = require("./allowedOrigins");
+const isInDevMode = process.env.NODE_ENV !== "production";
 
 // corsOptions function used in the /server.js file
 const corsOptions = {
@@ -7,9 +8,16 @@ const corsOptions = {
     // : (origin, ...) this is the browser origin of the request
     // the first arg searches our whitelist
     // the || !origin, allows cors localhost access to requests NOTE: remove (!origin) before deployment
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
+
+    // NOTE: the following is the original cors option callback
+    // if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    //   callback(null, true);
+    // } else {
+      // Adding production versus dev deployment option which removes cross origin restriction
+      if (isInDevMode && allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      }
+      else {
       callback(new Error("Not allowed by CORS"));
     }
   },
